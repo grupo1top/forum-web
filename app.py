03cb3353eba45a5d13859d9a50bf901ebdf7921c
nicklaranjeira <nicolle.laranjeira02@gmail.com>
2026-04-07 08:05:36 -0300

    intancia api

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi import Form
 from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
