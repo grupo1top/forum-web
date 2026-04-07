fca158b7f7a914127aee510f4288d0e6c6919ee8
nicklaranjeira <nicolle.laranjeira02@gmail.com>
2026-04-07 08:03:57 -0300

    imports

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("py")
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Form
+from fastapi.responses import RedirectResponse
